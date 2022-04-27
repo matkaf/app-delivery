e80d1c83ef1a2f5ccd8589909e08bc845b3072fe
Vitor Silva <vitor.o.silva95@gmail.com>
2022-04-27 19:29:24 -0300

    :sparkles: feat: Criando model sales

---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,62 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('sales', {
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull:false
+      },
+    totalPrice: {
+      type: DataTypes.DECIMAL(9,2),
+      allowNull: false
+    },
+    deliveryAddress: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deliveryNumber: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    saleDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true 
+    },
+    sellerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true 
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'sales',
+    underscored: true
+  });
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, {
+      as: 'users',
+      foreignKey: 'userId',
+    });
+  };
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, {
+      as: 'sellers',
+      foreignKey: 'sellerId',
+    });
+  };
+
+  return Sale;
+};
