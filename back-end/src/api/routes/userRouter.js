9384cf7cc7cd334de5ee91b50c2d6c7e1e96d990
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-28 11:55:55 -0300

    :sparkles: feat: Implementa rota 'DELETE /users/:id'

--- back-end/src/api/routes/userRouter.js
+++ back-end/src/api/routes/userRouter.js
@@ -1,6 +1,7 @@
 const user = require('express').Router();
 const userController = require('../../app/controllers/userController');
 
+user.delete('/:id', userController.deleteById);
 user.put('/:id', userController.update);
 user.get('/:id', userController.getById);
 user.post('/', userController.create);
