0b20af6db7d659bfd23854ce6247a27ed86660e4
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-28 11:49:35 -0300

    :sparkles: feat: Implementa rota 'PUT /users/:id'

--- back-end/src/api/routes/userRouter.js
+++ back-end/src/api/routes/userRouter.js
@@ -1,6 +1,7 @@
 const user = require('express').Router();
 const userController = require('../../app/controllers/userController');
 
+user.put('/:id', userController.update);
 user.get('/:id', userController.getById);
 user.post('/', userController.create);
 user.get('/', userController.getAll);
