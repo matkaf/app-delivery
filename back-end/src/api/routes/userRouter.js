7ca3dfeb2444b01ced1e0b465876d193f6543c5c
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-28 11:33:11 -0300

    :sparkles: feat: Implementa rota 'GET /users/:id'

--- back-end/src/api/routes/userRouter.js
+++ back-end/src/api/routes/userRouter.js
@@ -1,6 +1,7 @@
 const user = require('express').Router();
 const userController = require('../../app/controllers/userController');
 
+user.get('/:id', userController.getById);
 user.post('/', userController.create);
 user.get('/', userController.getAll);
 
