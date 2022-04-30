73e09fde6ed1f0d3771ab0f6b643775162bcc70b
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-30 09:37:56 -0300

    :sparkles: feat: Implementa um middleware de erros para capturar erros externos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,9 @@
 const cors = require('cors');
+require('express-async-errors');
 const express = require('express');
 const userRouter = require('./routes/userRouter');
 const loginRouter = require('./routes/loginRouter');
+const errorMiddleware = require('../app/middleware/errorMiddleware');
 
 const app = express();
 
@@ -13,5 +15,7 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/users', userRouter);
 app.use('/login', loginRouter);
+
+app.use(errorMiddleware);
 
 module.exports = app;
