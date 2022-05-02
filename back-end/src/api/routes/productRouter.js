07c575e8a617756265389701e8deb1dacdc008d2
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-02 16:53:01 -0300

    :sparkles: feat: Implementa rota 'GET /products/:id'

--- back-end/src/api/routes/productRouter.js
+++ back-end/src/api/routes/productRouter.js
@@ -1,6 +1,7 @@
 const product = require('express').Router();
 const productController = require('../../app/controllers/productController');
 
+product.get('/:id', productController.getById);
 product.post('/', productController.create);
 product.get('/', productController.getAll);
 
