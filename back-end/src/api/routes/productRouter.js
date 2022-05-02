dd8c80d07439894b963e56ee6e7c06cfa91b719f
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-02 17:11:42 -0300

    :sparkles: feat: Implementa rota 'DELETE /products/:id'

--- back-end/src/api/routes/productRouter.js
+++ back-end/src/api/routes/productRouter.js
@@ -1,6 +1,7 @@
 const product = require('express').Router();
 const productController = require('../../app/controllers/productController');
 
+product.delete('/:id', productController.deleteById);
 product.get('/:id', productController.getById);
 product.put('/:id', productController.update);
 product.post('/', productController.create);
