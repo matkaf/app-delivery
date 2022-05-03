9ea8cd7dcfe0acc9fef181e651c50b0dff5d0bc8
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-05-03 13:54:47 -0300

    to fix drinks context

---
+++ front-end/src/hooks/useDrinks.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import React, {
+  createContext,
+  useContext, useEffect, useMemo, useState,
+} from 'react';
+import { getDrinks } from '../services/request';
+
+const DrinksContext = createContext({});
+
+function DrinksProvider({ children }) {
+  const [data, setData] = useState([]);
+  const getDrinksfromApi = async () => {
+    const drinks = await getDrinks();
+    setData(drinks);
+  };
+
+  useEffect(() => {
+    getDrinksfromApi();
+  }, []);
+
+  const foo = useMemo(
+    () => ({
+      data,
+      setData,
+    }),
+    [data],
+  );
+
+  return <DrinksContext.Provider value={ foo }>{children}</DrinksContext.Provider>;
+}
+
+DrinksProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const useDrinks = () => {
+  const context = useContext(DrinksContext);
+
+  if (!context) {
+    throw new Error('useDrinks must be used within an DrinksProvider');
+  }
+
+  return context;
+};
+
+export { DrinksProvider, useDrinks };
