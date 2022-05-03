dbac4187e2faaebd23abce98d9e838f1c27fa2e6
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-03 20:17:31 -0300

    :sparkles: feat: Método getById de sales na camada controllers

--- back-end/src/app/controllers/saleController.js
+++ back-end/src/app/controllers/saleController.js
@@ -11,6 +11,11 @@ const saleController = {
     const allSales = await saleService.getAll();
     return res.status(StatusCodes.OK).json(allSales);
   },
+  getById: async (req, res) => {
+    const { id } = req.params;
+    const saleId = await saleService.getById(id);
+    return res.status(StatusCodes.OK).json(saleId);
+  },
 };
 
 module.exports = saleController;
