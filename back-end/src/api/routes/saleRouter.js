3887fbe3756fd2e329eaaad5006f362b0f44ece8
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-03 20:07:27 -0300

    :sparkles: feat: Implementa rota 'GET /sales'

--- back-end/src/api/routes/saleRouter.js
+++ back-end/src/api/routes/saleRouter.js
@@ -2,5 +2,6 @@ const sale = require('express').Router();
 const saleController = require('../../app/controllers/saleController');
 
 sale.post('/', saleController.create);
+sale.get('/', saleController.getAll);
 
 module.exports = sale;
