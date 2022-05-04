a5b3c61abd22030efc22ae6531a754657ade3518
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-04 10:10:43 -0300

    :sparkles: feat: Implementa rota 'DELETE /sales/:id'

--- back-end/src/api/routes/saleRouter.js
+++ back-end/src/api/routes/saleRouter.js
@@ -1,6 +1,7 @@
 const sale = require('express').Router();
 const saleController = require('../../app/controllers/saleController');
 
+sale.delete('/:id', saleController.deleteById);
 sale.put('/:id', saleController.update);
 sale.get('/:id', saleController.getById);
 sale.post('/', saleController.create);
