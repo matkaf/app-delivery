428a4100b8f193733d195131ac3f81eaaff1c440
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-06 18:57:43 -0300

    :recycle: refactor: Método create de sales para adicionar dados do pedido na tabela salesProducts

--- back-end/src/app/controllers/saleController.js
+++ back-end/src/app/controllers/saleController.js
@@ -1,11 +1,30 @@
+const Sequelize = require('sequelize');
 const { StatusCodes } = require('http-status-codes');
 const saleService = require('../services/saleService');
+const saleProductService = require('../services/saleProductService');
+const config = require('../../database/config/config');
+const { toConvertSale } = require('../functions/helpers');
+
+const sequelize = new Sequelize(config.development);
 
 const saleController = {
   create: async (req, res) => {
-    const { body } = req;
-    const newSale = await saleService.create(body);
-    return res.status(StatusCodes.CREATED).json(newSale);
+    const saleObj = toConvertSale(req.body);
+
+    await sequelize.transaction(async (t) => {
+      const { productsArray } = req.body;
+      const newSale = await saleService.create(saleObj, { transaction: t });
+
+      const allProducts = productsArray.map(({ id, quantity }) => (
+        saleProductService.create(
+          { saleId: newSale.id, productId: id, quantity }, { transaction: t },
+        )
+      ));
+
+      Promise.all(allProducts);
+
+      return res.status(StatusCodes.CREATED).json(newSale);
+    });
   },
   getAll: async (_req, res) => {
     const allSales = await saleService.getAll();
