af08095c30bcaa360b4aa1b4833c5e8729182650
Vitor Silva <vitor.o.silva95@gmail.com>
2022-05-06 11:31:53 -0300

    feat: implementa componente DeliveryAddress

--- front-end/src/components/DeliveryAddress/index.jsx
+++ front-end/src/components/DeliveryAddress/index.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { requestUsers } from '../../services/request';
 import { Label, Form, InputAddress, Container, Button } from './styledDeliveryAddress';
 
-const vendedores = ['vendedor1', 'vendedor2', 'vendedor3'];
-function DeliveryAddress() {
+function DeliveryAddress({ totalPrice }) {
+  const [users, setUsers] = useState([]);
+  const [orderSeller, setOrderSeller] = useState([]);
+  const [address, setAddress] = useState([]);
+  const [numberAnddress, setNumberAddress] = useState([]);
+
+  const handleClick = async () => {
+    const { token } = user;
+    const salePayload = {};
+  };
+
+  const fetchUsers = async () => {
+    try {
+      const endpoint = '/users';
+      const allUsers = await requestUsers(endpoint);
+      setUsers(allUsers.filter((el) => el.role === 'seller'));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       <h4>Detalhes e Endereço para Entrega</h4>
@@ -10,20 +34,23 @@ function DeliveryAddress() {
         <Form>
           <Label htmlFor="vendedor">
             P.vendedora Responsável
-            <select id="vendedor">
-              {vendedores.map((vendedor) => <option key={ vendedor }>{vendedor}</option>)}
+            <select id="vendedor" onChange={ (e) => setOrderSeller(e.target.value) }>
+              {
+                users
+                  .map((seller) => <option key={ seller.email }>{seller.name}</option>)
+              }
             </select>
           </Label>
           <Label htmlFor="adress">
             Endereço
-            <InputAddress type="text" />
+            <InputAddress type="text" onChange={ (e) => setAddress(e.target.value) } />
           </Label>
           <Label htmlFor="adress">
             Número
-            <input type="text" />
+            <input type="text" onChange={ (e) => setNumberAddress(e.target.value) } />
           </Label>
         </Form>
-        <Button type="button">FINALIZAR PEDIDO</Button>
+        <Button onclick={ handleClick } type="button">FINALIZAR PEDIDO</Button>
       </Container>
     </div>
   );
