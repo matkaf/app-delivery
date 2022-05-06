0ec9ecc517f07a091587a5b6c69706b2eb5e9cc7
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-06 18:59:09 -0300

    :sparkles: feat: Método toConvertSale nos helpers

--- back-end/src/app/functions/helpers.js
+++ back-end/src/app/functions/helpers.js
@@ -6,4 +6,19 @@ const passwordToHash = (userObj) => {
   return { ...userObj, password: passwordMd5 };
 };
 
-module.exports = passwordToHash;
+const toConvertSale = (saleObj) => {
+  const newSale = saleObj
+    .map(({ userId, sellerId, totalPrice, deliveryAddress, deliveryNumber }) => ({
+        userId,
+        sellerId,
+        totalPrice,
+        deliveryAddress,
+        deliveryNumber,
+      }));
+  return newSale;
+};
+
+module.exports = {
+  passwordToHash,
+  toConvertSale,
+};
