141897699b98bcf75ea963e8539664be26782324
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-05-06 15:12:03 -0300

    refact seeds

--- back-end/src/database/seeders/20220502201512-products.js
+++ back-end/src/database/seeders/20220502201512-products.js
@@ -6,27 +6,27 @@ module.exports = {
       {
         name:'Antarctica Pilsen 300ml',
         price: 4.99,
-        url_image: './back-end/public/antarctica_pilsen_300ml.jpg',
+        url_image: '/back-end/public/antarctica_pilsen_300ml.jpg',
       },
       {
         name:'Becks 330ml',
         price: 7.59,
-        url_image: './back-end/public/becks_330ml.jpg',
+        url_image: '/back-end/public/becks_330ml.jpg',
       },
       {
         name:'Becks 600ml',
         price: 10.90,
-        url_image: './back-end/public/becks_600ml.jpg',
+        url_image: '/back-end/public/becks_600ml.jpg',
       },
       {
         name:'Brahma 600ml',
         price: 6.99,
-        url_image: './back-end/public/brahma_600ml.jpg',
+        url_image: '/back-end/public/brahma_600ml.jpg',
       },
       {
         name:'Brahma Duplo Malte 350ml',
         price: 3.50,
-        url_image: './back-end/public/brahma_duplo_malte_350ml.jpg',
+        url_image: '/back-end/public/brahma_duplo_malte_350ml.jpg',
       },
     ], {}),
 
