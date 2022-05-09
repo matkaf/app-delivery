dbf25ed385317ff1ba572573f766068b571b1141
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-09 17:40:23 -0300

    :recycle: refactor: nome da tabela na migration de salesproducts

--- back-end/src/database/migrations/20220427214850-create-sales-products.js
+++ back-end/src/database/migrations/20220427214850-create-sales-products.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('salesProducts', {
+    await queryInterface.createTable('sales_products', {
       quantity: {
         allowNull: false,
         type: Sequelize.INTEGER
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, _Sequelize) {
-    await queryInterface.dropTable('salesProducts');
+    await queryInterface.dropTable('sales_products');
   }
 };
