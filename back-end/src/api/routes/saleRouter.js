dd33774319eb126e2bb3ce24b9a9b0225fe40861
Vitor Silva <vitor.o.silva95@gmail.com>
2022-05-09 20:16:47 -0300

    fix: middleware verifyJWT na rota post

--- back-end/src/api/routes/saleRouter.js
+++ back-end/src/api/routes/saleRouter.js
@@ -1,10 +1,11 @@
 const sale = require('express').Router();
+const { verifyJWT } = require('../../app/auth/generatorJwt');
 const saleController = require('../../app/controllers/saleController');
 
 sale.delete('/:id', saleController.deleteById);
 sale.put('/:id', saleController.update);
 sale.get('/:id', saleController.getById);
-sale.post('/', saleController.create);
+sale.post('/', verifyJWT, saleController.create);
 sale.get('/', saleController.getAll);
 
 module.exports = sale;
