a88df6d8078bd2909f8e1c7136e3c8cf2ff21ab3
Vitor Silva <vitor.o.silva95@gmail.com>
2022-05-11 19:30:21 -0300

    feat: rota admin

--- back-end/src/api/routes/userRouter.js
+++ back-end/src/api/routes/userRouter.js
@@ -1,7 +1,9 @@
 const user = require('express').Router();
 const userController = require('../../app/controllers/userController');
+const { verifyJWT, verifyRoleToken } = require('../../app/auth/generatorJwt');
 
 user.get('/search', userController.getByRole);
+user.post('/admin', verifyRoleToken, verifyJWT, userController.create);
 user.delete('/:id', userController.deleteById);
 user.put('/:id', userController.update);
 user.get('/:id', userController.getById);
